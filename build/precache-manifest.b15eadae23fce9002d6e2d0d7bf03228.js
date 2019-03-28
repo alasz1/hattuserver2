d7ddf53357dbb9a41ed54dcb8df6a3b5eb0582a2
inari a <iaaltojarvi@gmail.com>
2019-03-28 11:49:52 +0100

    final build

---
+++ build/precache-manifest.b15eadae23fce9002d6e2d0d7bf03228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b79baf8cabd6476a9d4",
+    "url": "/static/css/main.95bbbe43.chunk.css"
+  },
+  {
+    "revision": "4b79baf8cabd6476a9d4",
+    "url": "/static/js/main.f75d41ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76f7b53c2bb6528574f4",
+    "url": "/static/js/2.92203e41.chunk.js"
+  },
+  {
+    "revision": "c772c896db5e5fc156a01f2d431fc644",
+    "url": "/static/media/women.c772c896.jpg"
+  },
+  {
+    "revision": "3d406894ee85d4ebdc9164770c356b0e",
+    "url": "/index.html"
+  }
+];
